674cb96f5344bc6923afd447c0992a16d0789e71
aditya12gusain <adisandi3112002@gmail.com>
2022-09-22 21:22:56 +0530

    Completed all schemas

---
+++ server/database/review/index.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const ReviewSchema = mongoose.Schema(
+  {
+    food: { type: mongoose.Types.ObjectId, ref: "foods" },
+    restaurant: { type: mongoose.Types.ObjectId, ref: "restaurants" },
+    user: { type: mongoose.Types.ObjectId, ref: "users" },
+    rating: { type: Number, required: true },
+    reviewText: { type: String, required: true },
+    isRestaurantReview: Boolean,
+    isFoodReview: Boolean,
+    photos: {
+      type: mongoose.Types.ObjectId,
+      ref: "images",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const ReviewModel = mongoose.model("reviews", ReviewSchema);
--- server/index.js
+++ server/index.js
@@ -19,14 +19,12 @@ zomato.get("/", (req, res) => {
 const PORT = 4000;
 
 zomato.listen(PORT, () => {
-  // ConnectDB()
-  //   .then(() => {
-  //     console.log("Server is running !!!");
-  //   })
-  //   .catch((error) => {
-  //     console.log("Server is running, but database connection failed...");
-  //     console.log(error);
-  //   });
-
-  console.log("Server is running !!!");
+  ConnectDB()
+    .then(() => {
+      console.log("Server is running !!!");
+    })
+    .catch((error) => {
+      console.log("Server is running, but database connection failed...");
+      console.log(error);
+    });
 });

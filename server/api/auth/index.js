5b3c2058e77507ee814994f6c7f7b18b64a4461b
aditya12gusain <adisandi3112002@gmail.com>
2022-09-24 20:48:29 +0530

    Completed Auth Food and Restaurant routes

--- server/api/auth/index.js
+++ server/api/auth/index.js
@@ -7,7 +7,6 @@ const Router = express.Router();
 Router.post("/signup", async (req, res) => {
   try {
     await UserModel.findByEmailAndPhone(req.body.credentials);
-
     const newUser = await UserModel.create(req.body.credentials);
     const token = newUser.generateJwtToken();
     return res.status(200).json({ token, status: "success" });
@@ -16,6 +15,14 @@ Router.post("/signup", async (req, res) => {
   }
 });
 
-Router.post("/signin", async (req, res) => {});
+Router.post("/signin", async (req, res) => {
+  try {
+    const user = await UserModel.findByEmailAndPassword(req.body.credentials);
+    const token = user.generateJwtToken();
+    return res.status(200).json({ token, status: "success" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 export default Router;
---
+++ server/api/restaurant/index.js
@@ -0,0 +1,94 @@
+import express from "express";
+
+import { RestaurantModel } from "../../database/allModels";
+
+const Router = express.Router();
+
+/**
+ * Route     /
+ * Des       Create new restaurant
+ * Params    none
+ * Access    Public
+ * Method    POST
+ */
+// Homework
+
+/**
+ * Route     /
+ * Des       Get all the restuarant details based on the city
+ * Params    none
+ * Access    Public
+ * Method    GET
+ */
+Router.get("/", async (req, res) => {
+  try {
+    // http://localhost:4000/restaurant/?city=ncr
+    const { city } = req.query;
+    const restaurants = await RestaurantModel.find({ city });
+    if (restaurants.length === 0) {
+      return res.json({ error: "No restaurant found in this city." });
+    }
+    return res.json({ restaurants });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+/**
+ * Route     /:_id
+ * Des       Get individual restuarant details based on id
+ * Params    _id
+ * Access    Public
+ * Method    GET
+ */
+Router.get("/:_id", async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const restaurant = await RestaurantModel.findById(_id);
+
+    if (!restaurant) {
+      return res.status(400).json({ error: "Restaurant not found" });
+    }
+
+    return res.json({ restaurant });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+/**
+ * Route     /search/:searchString
+ * Des       Get restaurants details based on search string
+ * Params    searchString
+ * Access    Public
+ * Method    GET
+ */
+Router.get("/search/:searchString", async (req, res) => {
+  /**
+   * searchString = Raj
+   * results = {
+   *  RajHotel
+   *  RajRow
+   *  RonRaj
+   *  raJRow
+   * }
+   */
+  try {
+    const { searchString } = req.params;
+    const restaurants = await RestaurantModel.find({
+      name: { $regex: searchString, $options: "i" },
+    });
+
+    if (!restaurants.length === 0) {
+      return res
+        .status(404)
+        .json({ error: `No restaurant matched with ${searchString}` });
+    }
+
+    return res.json({ restaurants });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+export default Router;
